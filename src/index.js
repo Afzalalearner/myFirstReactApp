f113a1a9e584d560851e2276e15a936f6def4830
Afzal Ur Rahman <afzalalearner@gmail.com>
2023-01-11 00:44:29 +0530

    finally Ready for Deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM  from 'react-dom'
+import ReactDOM  from 'react-dom/client'
 
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -14,4 +14,10 @@ import App from './App'
 
 
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+// ReactDOM.render(<App/>,document.getElementById('root'))
+
+ReactDOM
+.createRoot(document.getElementById('root'))
+.render(<React.StrictMode>
+    <App/>
+    </React.StrictMode>)
